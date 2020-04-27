a629bbfd8b47501d672d6c8d508127d2a06bf08e
joisgori <00034715@uca.edu.sv>
2020-04-26 22:26:45 -0600

    ejercicio dos, terminado

--- tarea_2_00034715.asm
+++ tarea_2_00034715.asm
@@ -38,4 +38,18 @@
         mov     cl, "o"
         mov     [20Ch], cl
 
+        ;ejercicio 2, estimaciones de casos de covid-19
+
+        mov     bx, 0000h ;limpiando var
+        mov     cx, 0000h ;limpio para la variable que afecta el loop
+        mov     cx, 11d ;la cantidad de veces (las estimaciones) que se ejecutará el loop
+        mov     al, 2
+        mov     bl, 2 ;ya se se incrementa en dos
+        mov     si, 0 ;iniciando la variable que hará que se mueva en las celdas de memoria
+
+dupli:  MUL     bl
+        mov     [210h+si], ax
+        INC     si
+        loop    dupli
+        
         int     20h
